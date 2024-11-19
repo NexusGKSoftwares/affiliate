b40c3abc569a9de1dcb7c4129a8a2e3c4cb31420
NexusGKSoftwares <nexusgk3@gmail.com>
2024-11-19 11:57:43 +0300

    "Refactored TriviaQuestion class: removed constructor parameter, added global pdo, removed getAllQuestions, create, and getQuestionById methods, and added getRandomQuestions method."

--- models/TriviaQuestion.php
+++ models/TriviaQuestion.php
@@ -1,33 +1,17 @@
 <?php
+require_once '../config/database.php';
 
-class TriviaQuestion
-{
+class TriviaQuestion {
     private $db;
 
-    public function __construct($pdo)
-    {
+    public function __construct() {
+        global $pdo;
         $this->db = $pdo;
     }
 
-    // Fetch all trivia questions
-    public function getAllQuestions()
-    {
-        $stmt = $this->db->query("SELECT * FROM trivia_questions");
-        return $stmt->fetchAll();
-    }
-
-    // Add a new trivia question
-    public function create($question, $options, $correctAnswer)
-    {
-        $stmt = $this->db->prepare("INSERT INTO trivia_questions (question, options, correct_answer) VALUES (?, ?, ?)");
-        return $stmt->execute([$question, json_encode($options), $correctAnswer]);
-    }
-
-    // Get a trivia question by ID
-    public function getQuestionById($id)
-    {
-        $stmt = $this->db->prepare("SELECT * FROM trivia_questions WHERE id = ?");
-        $stmt->execute([$id]);
-        return $stmt->fetch();
+    public function getRandomQuestions($limit = 5) {
+        $stmt = $this->db->query("SELECT * FROM trivia_questions ORDER BY RAND() LIMIT $limit");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
+?>
