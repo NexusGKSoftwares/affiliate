da484831805a5031d21ccff3f90318a578c02419
NexusGKSoftwares <nexusgk3@gmail.com>
2024-11-19 11:32:32 +0300

    "Added UserController.php with login and register methods"

---
+++ controllers/UserController.php
@@ -0,0 +1,11 @@
+<?php
+class UserController {
+    public function login($username, $password) {
+        // Perform user authentication
+    }
+
+    public function register($data) {
+        // Handle user registration
+    }
+}
+?>
