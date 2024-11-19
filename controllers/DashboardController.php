5a2ba909c136210c453aa2fe2275a6df226eef9f
NexusGKSoftwares <nexusgk3@gmail.com>
2024-11-19 11:32:15 +0300

    "Added index method to DashboardController and modified getDashboardData method signature"

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -2,6 +2,12 @@
 require_once '../models/Transaction.php';
 
 class DashboardController {
+    public function index() {
+        // Fetch data for the dashboard
+        $user = $_SESSION['user_id'];
+        $transactions = Transaction::getTransactionsByUser($user);
+        require_once 'views/dashboard.php';
+    }
     public function getDashboardData($userId) {
         $transactionModel = new Transaction();
         $deposits = $transactionModel->getTotalDeposits($userId);
