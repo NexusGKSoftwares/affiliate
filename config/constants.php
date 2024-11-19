f00d84513a880e5e02c6c137ae6adc8df292a104
NexusGKSoftwares <nexusgk3@gmail.com>
2024-11-19 11:29:35 +0300

    "Updated SITE_NAME constant and added ADMIN_EMAIL constant in config/constants.php"

--- config/constants.php
+++ config/constants.php
@@ -1,4 +1,5 @@
 <?php
-define('SITE_NAME', 'Ecolab Marketing System');
 define('BASE_URL', 'http://localhost/online_marketing_system/');
+define('SITE_NAME', 'Online Marketing System');
+define('ADMIN_EMAIL', 'admin@example.com');
 ?>
