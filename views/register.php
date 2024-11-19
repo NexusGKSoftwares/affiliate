59f0deae3cc54a4a8c7bb6aabbefe741d0fa0e31
NexusGKSoftwares <nexusgk3@gmail.com>
2024-11-19 11:21:14 +0300

    "Added HTML form for user registration to register.php"

--- views/register.php
+++ views/register.php
@@ -14,3 +14,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 }
 ?>
+<form action="register.php" method="POST">
+    <h2>Register</h2>
+    <input type="text" name="username" placeholder="Username" required>
+    <input type="email" name="email" placeholder="Email" required>
+    <input type="password" name="password" placeholder="Password" required>
+    <button type="submit">Register</button>
+</form>
