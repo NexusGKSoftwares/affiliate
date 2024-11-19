5d478f6cc12ae4609c8d13f538a4c9a5cc424439
NexusGKSoftwares <nexusgk3@gmail.com>
2024-11-19 12:02:17 +0300

    "Added sidebar.php with basic menu structure"

---
+++ views/layout/sidebar.php
@@ -0,0 +1,10 @@
+<aside class="sidebar">
+    <ul class="sidebar-menu">
+        <li><a href="dashboard.php"><i class="icon-dashboard"></i> Dashboard</a></li>
+        <li><a href="transactions.php"><i class="icon-transactions"></i> Transactions</a></li>
+        <li><a href="trivia.php"><i class="icon-trivia"></i> Trivia</a></li>
+        <li><a href="jobs.php"><i class="icon-jobs"></i> Jobs</a></li>
+        <li><a href="profile.php"><i class="icon-profile"></i> Profile</a></li>
+        <li><a href="logout.php"><i class="icon-logout"></i> Logout</a></li>
+    </ul>
+</aside>
