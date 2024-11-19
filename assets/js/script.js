6b91b1af5e45da5f20b8fbea066c56c8b3ec0313
NexusGKSoftwares <nexusgk3@gmail.com>
2024-11-19 11:44:42 +0300

    "Updated fetch request to POST method with JSON payload"

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ fetch('/api/trivia.php')
     .then(response => response.json())
     .then(data => console.log(data));
 
-    fetch('/api/trivia.php', {
+fetch('/api/trivia.php', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ question_id: 1, selected_option: 'A' })
